c2d3fc70c02defd69d1e32886c81d65eb0086c80
RenewDo <2445382766@qq.com>
2016-08-08 10:09:06 +0800

    Create 61、Binary Tree Level Order Traversal.cpp

---
+++ algorithm/Leetcode/61、Binary Tree Level Order Traversal.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        queue<TreeNode*> cur,next;
+        if(root)
+              cur.push(root);
+        else  return res;
+        while(!cur.empty())
+        {
+            vector<int> level;
+            while(!cur.empty())
+            {
+                TreeNode * tmp= cur.front();
+                cur.pop();
+                level.push_back(tmp->val);
+                if(tmp->left)
+                next.push(tmp->left);
+                if(tmp->right)
+                next.push(tmp->right);
+            }
+            
+            res.push_back(level);
+            swap(next, cur);
+        }
+        return res;
+    }
+};
